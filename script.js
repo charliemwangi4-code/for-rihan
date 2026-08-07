1e93042df397840c3062f51fc1afcd29547841d4
charliemwangi4-code <charliemwangi4@gmail.com>
2026-08-07 21:06:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const hearts = document.getElementById("hearts");
 
+function createHeart(){
+
+    const heart = document.createElement("div");
+
+    heart.classList.add("heart");
+
+    heart.innerHTML = "💕";
+
+    heart.style.left = Math.random()*100+"vw";
+
+    heart.style.animationDuration =
+    (4+Math.random()*4)+"s";
+
+    heart.style.fontSize =
+    (20+Math.random()*18)+"px";
+
+    hearts.appendChild(heart);
+
+    setTimeout(()=>{
+
+        heart.remove();
+
+    },8000);
+
+}
+
+setInterval(createHeart,350);
