1e45a9d618429df6ca3b8a8a71bc17d542d4c95a
charliemwangi4-code <charliemwangi4@gmail.com>
2026-08-07 22:44:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,95 @@
 const hearts = document.getElementById("hearts");
 
+let paused = false;
+
 function createHeart(){
+
+    if(paused) return;
 
     const heart = document.createElement("div");
 
     heart.classList.add("heart");
 
-    const emojis = ["🥹","💕","🫶🏽"];
+    const emojis=["🥹","💕","🫶🏽"];
 
-heart.innerHTML =
-emojis[Math.floor(Math.random()*emojis.length)];
-    heart.style.left = Math.random()*100+"vw";
+    heart.innerHTML=emojis[Math.floor(Math.random()*emojis.length)];
 
-    heart.style.animationDuration =
-    (4+Math.random()*4)+"s";
+    heart.style.left=Math.random()*100+"vw";
 
-    heart.style.fontSize =
-    (20+Math.random()*18)+"px";
+    heart.style.animationDuration=(4+Math.random()*4)+"s";
+
+    heart.style.fontSize=(20+Math.random()*18)+"px";
 
     hearts.appendChild(heart);
 
-    setTimeout(()=>{
-
-        heart.remove();
-
-    },8000);
-
+    setTimeout(()=>heart.remove(),8000);
 }
 
 setInterval(createHeart,350);
+
+const hero=document.querySelector(".hero");
+const intro=document.getElementById("intro");
+const start=document.getElementById("startButton");
+
+const typing=document.getElementById("typing");
+const typing2=document.getElementById("typing2");
+const continueBtn=document.getElementById("continueBtn");
+
+start.onclick=()=>{
+
+    start.style.transform="scale(.88)";
+
+    paused=true;
+
+    hero.style.transition="1s";
+    hero.style.opacity="0";
+
+    setTimeout(()=>{
+
+        hero.style.display="none";
+
+        intro.classList.remove("hidden");
+
+        requestAnimationFrame(()=>{
+            intro.classList.add("show");
+        });
+
+        typeText(
+            "Hi babyyyy RayRay 🥹💕",
+            typing,
+            ()=>{
+                typeText(
+                    "Welcome to our little world...",
+                    typing2,
+                    ()=>{
+                        continueBtn.style.display="inline-block";
+                    }
+                );
+            }
+        );
+
+    },1000);
+
+};
+
+function typeText(text,element,callback){
+
+    let i=0;
+
+    const timer=setInterval(()=>{
+
+        element.innerHTML+=text.charAt(i);
+
+        i++;
+
+        if(i>=text.length){
+
+            clearInterval(timer);
+
+            if(callback) callback();
+
+        }
+
+    },65);
+
+}
